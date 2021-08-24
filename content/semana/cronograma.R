c5eb26e8efa37cc5234991e8ac6750fb181bda87
Fernando Mayer <fernandomayer@gmail.com>
2021-08-24 00:04:34 -0300

    Inclui apresentacao de trabalhos na tabela de terça

--- content/semana/cronograma.R
+++ content/semana/cronograma.R
@@ -13,10 +13,10 @@ dd |>
     kable_styling(bootstrap_options = c("striped", "bordered", "responsive")) |>
     ## kable_material(c("striped", "hover")) |>
     pack_rows("Segunda-feira (13/09)", 1, 3, color = "3f3f3f", background = "lightgrey") |>
-    pack_rows("Terça-feira (14/09)", 4, 4, color = "3f3f3f", background = "lightgrey") |>
-    pack_rows("Quarta-feira (15/09)", 5, 7, color = "3f3f3f", background = "lightgrey") |>
-    pack_rows("Quinta-feira (16/09)", 8, 10, color = "3f3f3f", background = "lightgrey") |>
-    pack_rows("Sexta-feira (17/09)", 11, 13, color = "3f3f3f", background = "lightgrey") |>
+    pack_rows("Terça-feira (14/09)", 4, 5, color = "3f3f3f", background = "lightgrey") |>
+    pack_rows("Quarta-feira (15/09)", 6, 8, color = "3f3f3f", background = "lightgrey") |>
+    pack_rows("Quinta-feira (16/09)", 9, 11, color = "3f3f3f", background = "lightgrey") |>
+    pack_rows("Sexta-feira (17/09)", 12, 14, color = "3f3f3f", background = "lightgrey") |>
     column_spec(1:2, color = "3f3f3f") |>
     save_kable("content/semana/crono.html")
 
