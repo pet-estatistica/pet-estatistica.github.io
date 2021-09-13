cda653ec4254599bbe2c9fe197c2e7c636ff24e3
Fernando Mayer <fernandomayer@gmail.com>
2021-09-13 11:53:21 -0300

    Aumenta frame

--- content/semana/cronograma.R
+++ content/semana/cronograma.R
@@ -12,7 +12,8 @@ str(dd)
 dd |>
     dplyr::select(Hora, Atividade, Link) |>
     kable(col.names = NULL) |>
-    kable_styling(bootstrap_options = c("striped", "bordered", "responsive")) |>
+    kable_styling(bootstrap_options = c("striped", "bordered"),
+                  full_width = FALSE, position = "center") |>
     ## kable_material(c("striped", "hover")) |>
     ## collapse_rows(columns = 3, valign = "middle") |>
     pack_rows("Segunda-feira (13/09)", 1, 3, color = "3f3f3f", background = "lightgrey") |>
@@ -20,7 +21,7 @@ dd |>
     pack_rows("Quarta-feira (15/09)", 6, 8, color = "3f3f3f", background = "lightgrey") |>
     pack_rows("Quinta-feira (16/09)", 9, 11, color = "3f3f3f", background = "lightgrey") |>
     pack_rows("Sexta-feira (17/09)", 12, 14, color = "3f3f3f", background = "lightgrey") |>
-    column_spec(1:2, color = "3f3f3f") |>
+    column_spec(1:3, color = "3f3f3f") |>
     save_kable("content/semana/crono.html")
 
 ## Prazos
